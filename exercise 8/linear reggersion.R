52ab37d6ef9969b0224d431dd42b1d1f20f5dd99
Pallavi reddy <113233380+Reddypallavireddy@users.noreply.github.com>
2023-01-12 00:06:53 -0800

    Create linear reggersion.R

---
+++ exercise 8/linear reggersion.R
@@ -0,0 +1,10 @@
+diabetes=read.csv("D:\\folders\\DWHDM\\diabetes.csv")
+
+# All the steps must be written in the console to get the output
+
+#step 1 PLOT
+relation=lm(diabetes$BloodPressure~diabetes$Age)
+png=(file="linearregression.png")
+plot(diabetes$Age, diabetes$BloodPressure)
+
+#step 2 RESULT
